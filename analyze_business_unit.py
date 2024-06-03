4b60ce21e50826e5df29d7f5ebcf60a60b3a4c3c
Luke Shinn <luke.shinn.efp@gmail.com>
2024-06-03 12:19:43 -0500

    handle rest and bifrost indexing

--- analyze_business_unit.py
+++ analyze_business_unit.py
@@ -13,32 +13,30 @@ logger = logging.getLogger(__name__)
 def analyze_business_unit(api_format, date_to_run_analysis_on):
     command = ['pyhaat-coverage', api_format, '-l', '-rg', 'hudl_repos/competitive']
     print(api_format)
-    print(f"======== Running {command} ========")
-    print("============ this could take up to a minute ============")
+    print(f"==== Running {command} ====")
+    print(f"==== this could take a bit to run..... ====")
     bulk_coverage_output = subprocess.run(command, stdout=subprocess.PIPE).stdout.decode('utf-8').replace("'", '"')
     # TODO: CRITICAL: Handle "No endpoints found for {serviceName}; skipping coverage check"
     # print("==== outputting bulk coverage ====")
     # print(bulk_coverage_output)
     bulk_coverage_dictionary = json.loads(bulk_coverage_output)
     data = bulk_coverage_dictionary['services']
-    csv_file_path = f'metrics.csv'
+    csv_file_path = f'{api_format}metrics.csv'
     if path.exists(csv_file_path):
         data = pd.read_csv(csv_file_path)
-        display(data)
         incomming_data = pd.DataFrame(data)
-        incomming_data['run_date'] = date_to_run_analysis_on
-        display(incomming_data)
-        data = pd.concat([data, incomming_data], join="inner")
-        display(data)
+        format_dataframe_for_csv(incomming_data, api_format, date_to_run_analysis_on)
+        formatted_data = pd.concat([data, incomming_data], join="inner")
     else:
-        # formatted_data = [{'service_name': node['service_name'], **{key: value for key, value in node.items() if key != 'service_name'}} for node in data]
         data = pd.DataFrame(data=data)
-        data['run_date'] = date_to_run_analysis_on
-        display(data)
-        data = data.reindex(columns=['service_name', 'run_date', 'rest_api_cov', 'rest_endpoint_hit_rate'])
-        display(data)
-    data.to_csv(csv_file_path, index=False)
+        formatted_data = format_dataframe_for_csv(data, api_format, date_to_run_analysis_on)
+    formatted_data.to_csv(csv_file_path, index=False)
 
 
 def format_dataframe_for_csv(data, api_format, date_to_run_analysis_on):
-    print("works")
+    data['run_date'] = date_to_run_analysis_on
+    if api_format == "-r":
+        indexed_data = data.reindex(columns=['service_name', 'run_date', 'rest_api_cov', 'rest_endpoint_hit_rate'])
+    if api_format == "-b":
+        indexed_data = data.reindex(columns=['service_name', 'run_date', 'bifrost_api_cov', 'bifrost_endpoint_hit_rate'])
+    return indexed_data
--- todo.md
+++ todo.md
@@ -2,5 +2,4 @@ Critical: if "No endpoints found for hudl-organizations; skipping coverage check
 Critical: master vs main on some repos
 Add args params several places instead of hard coding business units/dates/etc
 if the repo older than the date provided that could cause wierdness. Unsure of behavior
-write new csv file with name of date ran instead of appending to test.csv
-
+General code quality in analyze_business_unit.py. Namely why am I having to initiate new varaibles instead of perserving one data frame
