9625ba46f27f307730b698d2d541598260765d93
Luke Shinn <luke.shinn.efp@gmail.com>
2024-06-04 08:54:54 -0500

    fix bug where variable was being reassigned

--- analyze_business_unit.py
+++ analyze_business_unit.py
@@ -5,10 +5,12 @@ import logging
 import pandas as pd
 import os.path as path
 
-# from IPython.display import display
+from IPython.display import display
 
 
 logger = logging.getLogger(__name__)
+
+debug = True
 
 
 def analyze_business_unit(api_format, date_to_run_analysis_on):
@@ -18,18 +20,16 @@ def analyze_business_unit(api_format, date_to_run_analysis_on):
     print(f"==== this could take a bit to run..... ====")
     bulk_coverage_output = subprocess.run(command, stdout=subprocess.PIPE).stdout.decode('utf-8').replace("'", '"')
     # TODO: CRITICAL: Handle "No endpoints found for {serviceName}; skipping coverage check"
-    # print("==== outputting bulk coverage ====")
-    # print(bulk_coverage_output)
     bulk_coverage_dictionary = json.loads(bulk_coverage_output)
-    data = bulk_coverage_dictionary['services']
+    services_json_data = bulk_coverage_dictionary['services']
     csv_file_path = f'{api_format}metrics.csv'
     if path.exists(csv_file_path):
-        data = pd.read_csv(csv_file_path)
-        incomming_data = pd.DataFrame(data)
+        csv_data = pd.read_csv(csv_file_path)
+        incomming_data = pd.DataFrame(services_json_data)
         format_dataframe_for_csv(incomming_data, api_format, date_to_run_analysis_on)
-        formatted_data = pd.concat([data, incomming_data], join="inner")
+        formatted_data = pd.concat([csv_data, incomming_data], join="inner")
     else:
-        data = pd.DataFrame(data=data)
+        data = pd.DataFrame(data=services_json_data)
         formatted_data = format_dataframe_for_csv(data, api_format, date_to_run_analysis_on)
     formatted_data.to_csv(csv_file_path, index=False)
 
